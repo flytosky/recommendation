0e9e0acff8da810e32e7d2b6edfc97298bcf4644
Lijun Xue <xljroy@gmail.com>
2016-01-28 00:14:28 -0800

    finish all

--- app.py
+++ app.py
@@ -14,10 +14,10 @@ app.config['MYSQL_DATABASE_HOST'] = "localhost"
 
 @app.route('/serviceRecommodation', methods=['POST'])
 def main():
-    print request.data
-    string = request.data
+    print request.json.get('keywords')
+    string = request.json.get('keywords')
     strList = string.split(' ')
-    seriveId = serviceLDA.getService(strList)
+    serviceId = serviceLDA.getService(strList)
     cursor = mysql.get_db().cursor()
     sql = "SELECT id, name, url FROM ClimateService WHERE id=%d"%(int(seriveId))
     cursor.execute(sql)
