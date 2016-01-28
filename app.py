a3bdb3664469906ad10c3bd7a43da4603500ce66
Lijun Xue <xljroy@gmail.com>
2016-01-28 00:30:07 -0800

    modify config

--- app.py
+++ app.py
@@ -7,19 +7,19 @@ app = Flask(__name__)
 
 mysql = MySQL()
 app.config['MYSQL_DATABASE_USER'] = "root"
-app.config['MYSQL_DATABASE_PASSWORD'] = ""
+app.config['MYSQL_DATABASE_PASSWORD'] = "root"
 app.config['MYSQL_DATABASE_DB'] = "climateService"
 app.config['MYSQL_DATABASE_HOST'] = "localhost"
 
 
-@app.route('/serviceRecommodation', methods=['POST'])
+@app.route('/serviceRecommendation', methods=['POST'])
 def main():
     print request.json.get('keywords')
     string = request.json.get('keywords')
     strList = string.split(' ')
     serviceId = serviceLDA.getService(strList)
     cursor = mysql.get_db().cursor()
-    sql = "SELECT id, name, url FROM ClimateService WHERE id=%d"%(int(seriveId))
+    sql = "SELECT id, name, url FROM ClimateService WHERE id=%d"%(int(serviceId))
     cursor.execute(sql)
     res = cursor.fetchall()[0]
     return jsonify(id=res[0], name=res[1], url=res[2])
